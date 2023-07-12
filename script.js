e6e346699c8e2e31cfa53270e9a2ace2ed27d5bc
thanabordeee <jejekup00123@hotmail.co.th>
2023-07-12 17:29:53 +0700

    complete

--- script.js
+++ script.js
@@ -3,4 +3,74 @@ function validateEmail(email) {
   var atPos = email.indexOf("@");
   var dotPos = email.lastIndexOf(".");
   return atPos > 0 && dotPos > atPos + 1 && dotPos < email.length - 1;
+}const firstNameInput = document.querySelector("#first-name-input");
+const lastNameInput = document.querySelector("#last-name-input");
+const emailInput = document.querySelector("#email-input");
+const passInput = document.querySelector("#password-input");
+const submitBtn = document.querySelector("#submit-btn");
+
+//simple email validation
+function validateEmail(email) {
+  var atPos = email.indexOf("@");
+  var dotPos = email.lastIndexOf(".");
+  return atPos > 0 && dotPos > atPos + 1 && dotPos < email.length - 1;
 }
+
+firstNameInput.onkeyup = () => {
+  firstNameInput.classList.remove("is-valid");
+  firstNameInput.classList.remove("is-invalid");
+};
+
+lastNameInput.onkeyup = () => {
+  lastNameInput.classList.remove("is-valid");
+  lastNameInput.classList.remove("is-invalid");
+};
+
+emailInput.onkeyup = () => {
+  emailInput.classList.remove("is-valid");
+  emailInput.classList.remove("is-invalid");
+};
+
+passInput.onkeyup = () => {
+  passInput.classList.remove("is-valid");
+  passInput.classList.remove("is-invalid");
+};
+
+submitBtn.onclick = () => {
+  let isFirstNameOk = false;
+  let isLastNameOk = false;
+  let isEmailOk = false;
+  let isPassOk = false;
+
+  if (firstNameInput.value === "") {
+    firstNameInput.classList.add("is-invalid");
+  } else {
+    firstNameInput.classList.add("is-valid");
+    isFirstNameOk = true;
+  }
+
+  if (lastNameInput.value === "") {
+    lastNameInput.classList.add("is-invalid");
+  } else {
+    lastNameInput.classList.add("is-valid");
+    isLastNameOk = true;
+  }
+
+  if (validateEmail(emailInput.value) === false) {
+    emailInput.classList.add("is-invalid");
+  } else {
+    emailInput.classList.add("is-valid");
+    isEmailOk = true;
+  }
+
+  if (passInput.value.length < 6) {
+    passInput.classList.add("is-invalid");
+  } else {
+    passInput.classList.add("is-valid");
+    isPassOk = true;
+  }
+
+  if (isFirstNameOk && isLastNameOk && isEmailOk && isPassOk) {
+    alert("Registered successfully");
+  }
+};
